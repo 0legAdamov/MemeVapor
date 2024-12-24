fdd5cc4abf573642933bceac61efa34288b7eadc
Oleg Adamov <oleg.adamov@corp.mail.ru>
2024-12-24 18:17:33 +0300

    update run_vapor

--- run_vapor.sh
+++ run_vapor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running database migration..."
-swift run App migrate
+#echo "Running database migration..."
+#swift run App migrate
 
 echo "Running app..."
 swift run App serve --hostname 0.0.0.0 --port 80
